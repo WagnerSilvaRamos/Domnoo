6707135566e3dce5db487b13fb4d24ee0690da06
wagner ramos <h@des666>
2019-11-20 15:13:45 -0300

    inicio projeto

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+    <comp-header/>
+  <div>content</div>
+    <comp-footer/>
+  </div>
+</template>
+
+<script>
+
+  import compHeader from './components/Header-footer/Header.vue';
+  import compFooter from './components/Header-footer/Footer.vue';
+
+export  default {
+components:{
+  compHeader,
+  compFooter
+}
+}
+</script>
+
+<style>
+  @import  './assets/style.css';
+</style>
