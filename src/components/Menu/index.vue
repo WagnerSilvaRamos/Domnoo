2e31bb825c61dd35ce6aad400f654c8b18a11730
wagner ramos <h@des666>
2019-11-21 19:15:31 -0300

    lista produtos

--- src/components/Menu/index.vue
+++ src/components/Menu/index.vue
@@ -8,12 +8,37 @@
             <md-button
             v-for="(button, index) in buttons"
             :key="index"
-            :class="button.active ? 'md-raised': '' "
-            @click="changeMenu(button.name)"
+               @click="changeMenu(button.name)"
+            :class="button.active ? 'md-raised' : ' ' "
+                  
 
             >
                 {{button.name}}
             </md-button>
+        </div>
+        <div class="products_items">
+            <md-card
+            v-for="(product, index) in products"
+            :key="index"
+             v-show="product.type === activeProduct"
+            >
+                <md-card-media md-ratio="16:9">
+                    <img :src="require(`../../assets/images/products/${product.img}`)"/>
+                </md-card-media>
+
+                <md-card-header>
+                    <h2 class="md-title">{{product.name}}</h2>
+                    <div class="md-subhead">
+                    <span>${{product.price}}</span>
+
+                    </div>
+                </md-card-header>
+
+<md-card-content>
+    {{product.desc}}
+</md-card-content>
+
+            </md-card>
         </div>
     </div>
 
@@ -28,10 +53,10 @@
                     {name:'pizza', active:true},
                     {name:'burgers', active:false},
                     {name:'salat', active:false},
-                    {name:'desert', active:false}
-
-
-                ]
+                    {name:'dessert', active:false}
+               ],
+              activeProduct:'pizza',
+             products: [ ]
             }
         },
         methods:{
@@ -39,13 +64,42 @@
                 this.buttons.forEach( item =>{
                     if(item.name == value){
                         item.active = true;
+                        this.activeProduct = value
                     }else{
                         item.active = false;
                     }
                 })
             }
-        }
+        },
+        created(){
+            this.$http.get('products.json')
+                .then(response => response.json())
+                .then( data => {
+                let list = [];
 
+                for(let key in data){
+                      list.push({
+                        ...data[key],
+                        id: key
+                    })
+                }
+                this.products = list
+           })
+        }
     }
 
 </script>
+
+<style>
+    .md-card{
+        width: 32%;
+        margin-bottom: 20px;
+    }
+
+    .products_items{
+        display: flex;
+         justify-content: space-between; 
+         flex-wrap: wrap
+        }
+
+</style>
