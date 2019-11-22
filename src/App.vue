12e8f407a6f951848799072815e2ede076918af6
wagner ramos <h@des666>
2019-11-22 13:52:54 -0300

    Domnoo Animation

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@
   <div>
 <comp-featured/>
       <comp-menu/>
+      <comp-call-us />
+
   </div>
 
 
@@ -20,13 +22,15 @@
   import compFooter from './components/Header-footer/Footer.vue';
   import compFeatured from './components/Featured/index.vue';
   import compMenu from './components/Menu/index.vue';
+  import compCallUs from './components/CallUs/index.vue';
 
 export  default {
 components:{
   compHeader,
   compFooter,
   compFeatured,
-  compMenu
+  compMenu,
+  compCallUs
 
 }
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource';
+import VueScrollReveal from 'vue-scroll-reveal';
 
 import { MdButton, MdDrawer, MdCard} from 'vue-material/dist/components'
 import 'vue-material/dist/vue-material.min.css'
@@ -11,6 +12,13 @@ Vue.use(VueResource)
 Vue.use(MdButton);
 Vue.use(MdDrawer);
 Vue.use(MdCard);
+
+Vue.use(VueScrollReveal,{
+duration:800 ,
+scale:1,
+distance:'50px'
+
+  });
 
 
 Vue.http.options.root = 'https://domnoo-wagner.firebaseio.com/'
